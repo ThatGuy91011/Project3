5627bfe18ada32b3017f2ffe5bf4e157b8c385d1
KIRK WALLS <KIRWALLS@uat.edu>
2019-10-20 14:43:08 -0700

    Initial Commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Transform tf;
+
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tf = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            tf.rotation = Quaternion.Euler(0, 0, 270);
+            tf.position += tf.up * speed;
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            tf.rotation = Quaternion.Euler(0, 0, 0);
+            tf.position += tf.up * speed;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            tf.rotation = Quaternion.Euler(0, 0, 90);
+            tf.position += tf.up * speed;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            tf.rotation = Quaternion.Euler(0, 0, 180);
+            tf.position += tf.up * speed;
+        }
+    }
+}
